533db2503c45e9c5352976c4c20f9850156259a3
Reynaldo Villar Garavini <67124541+ReynaldoGaravini@users.noreply.github.com>
2021-09-02 10:34:06 -0300

    .

--- src/Main.java
+++ src/Main.java
@@ -2,107 +2,115 @@ import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args) {
+
+
         try {
             CRUD crud = new CRUD();
             Scanner in = new Scanner(System.in);
             int menu;
 
             do{
-            System.out.println("1- Inserir Cliente");
-            System.out.println("2- Imprimir Cliente");
-            System.out.println("3- Atualizar Cliente");
-            System.out.println("4- Deletar CLiente");
-            System.out.println("0- Sair");
+                System.out.println("1- Inserir Cliente");
+                System.out.println("2- Imprimir Cliente");
+                System.out.println("3- Atualizar Cliente");
+                System.out.println("4- Deletar CLiente");
+                System.out.println("0- Sair");
 
-            menu = in.nextInt();
+                menu = in.nextInt();
 
-            switch(menu) {
-                //Inserir
-                case 1: 
-                    //ler cpf
-                    System.out.print("CPF: ");
-                    int cpf1 = in.nextInt();
-                    System.out.print("\n");
-                    //ler nome
-                    System.out.print("Nome: ");
-                    String nome1 = in.nextLine();
-                    System.out.print("\n");
-                    //ler email
-                    System.out.print("Email: ");
-                    String email1 = in.nextLine();
-                    System.out.print("\n");
+                switch(menu) {
+                    //Inserir
+                    case 1: 
+                        //ler cpf
+                        System.out.print("CPF: ");
+                        int cpf1 = in.nextInt();
+                        System.out.print("\n");
+                        //ler nome
+                        System.out.print("Nome: ");
+                        String nome1 = in.nextLine();
+                        System.out.print("\n");
+                        //ler email
+                        System.out.print("Email: ");
+                        String email1 = in.nextLine();
+                        System.out.print("\n");
 
-                    //cria objeto cliente
-                    Cliente c1 = new Cliente(-1, cpf1, nome1, email1);
-                    
-                    //função inserir do CRUD passando o novo cliente como parâmetro
-                    crud.create(c1);
+                        //cria objeto cliente
+                        Cliente c1 = new Cliente(-1, cpf1, nome1, email1);
+                        
+                        //função inserir do CRUD passando o novo cliente como parâmetro
+                        crud.create(c1);
 
-                    break;
-                //Imprimir
-                case 2:
-                    //ler id
-                    System.out.print("ID: ");
-                    int id2 = in.nextInt();
-                    System.out.print("\n");
+                        break;
+                    //Imprimir
+                    case 2:
+                        //ler id
+                        System.out.print("ID: ");
+                        int id2 = in.nextInt();
+                        System.out.print("\n");
 
-                    //função imprimir do CRUD
-                    Cliente c2 = crud.read(id2);
+                        //função imprimir do CRUD
+                        Cliente c2 = crud.read(id2);
 
-                    //imprime informações do cliente na tela
-                    System.out.print(c2.imprimeCliente());
-                    break;
-                //Atualizar
-                case 3: 
-                    //ler cpf
-                    System.out.print("CPF: ");
-                    int cpf3 = in.nextInt();
-                    System.out.print("\n");
-                    //ler nome
-                    System.out.print("Nome: ");
-                    String nome3 = in.nextLine();
-                    System.out.print("\n");
-                    //ler email
-                    System.out.print("Email: ");
-                    String email3 = in.nextLine();
-                    System.out.print("\n");
+                        //imprime informações do cliente na tela
+                        System.out.println(c2.imprimeCliente());
+                        break;
+                    //Atualizar
+                    case 3: 
+                        //ler id
+                        System.out.print("ID: ");
+                        int id3 = in.nextInt();
+                        System.out.print("\n");
+                        //ler cpf
+                        System.out.print("CPF: ");
+                        int cpf3 = in.nextInt();
+                        System.out.print("\n");
+                        //ler nome
+                        System.out.print("Nome: ");
+                        String nome3 = in.nextLine();
+                        System.out.print("\n");
+                        //ler email
+                        System.out.print("Email: ");
+                        String email3 = in.nextLine();
+                        System.out.print("\n");
 
-                    //cria objeto cliente
-                    Cliente c3 = new Cliente(-1, cpf3, nome3, email3);
+                        //cria objeto cliente
+                        Cliente c3 = new Cliente(id3, cpf3, nome3, email3);
 
-                    //função atualizar CRUD passando o novo cliente como parâmetro
-                    crud.update(c3);
+                        //função atualizar CRUD passando o novo cliente como parâmetro
+                        crud.update(c3);
 
-                    break;
-                //Deletar
-                case 4:
-                    //ler id
-                    System.out.print("ID: ");
-                    int id4 = in.nextInt();
-                    System.out.print("\n");
+                        break;
+                    //Deletar
+                    case 4:
+                        //ler id
+                        System.out.print("ID: ");
+                        int id4 = in.nextInt();
+                        System.out.print("\n");
 
-                    //função delete CRUD
-                    boolean deletado = crud.delete(id4);
+                        //função delete CRUD
+                        boolean deletado = crud.delete(id4);
 
-                    if(deletado) {
-                        System.out.println("Cliente deletado com sucesso!");
-                    } else {
-                        System.out.println("Erro ao deletar cliente!");
-                    }
+                        if(deletado) {
+                            System.out.println("Cliente deletado com sucesso!");
+                        } else {
+                            System.out.println("Erro ao deletar cliente!");
+                        }
 
-                    break;
-                //Sair
-                case 0: 
-                    //mensagem de saída do programa
-                    System.out.println("Programa Encerrado!");
-                    break;
-                //Erro - Número Inválido
-                default:
-                    System.out.println("Número Inválido!");
-                    break;
-            }
+                        break;
+                    //Sair
+                    case 0: 
+                        //mensagem de saída do programa
+                        System.out.println("Programa Encerrado!");
+                        break;
+                    //Erro - Número Inválido
+                    default:
+                        System.out.println("Número Inválido!");
+                        break;
+                }
 
-        } while(menu!=0);
+            } while(menu!=0);
+
+            in.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
